338fb070d2a71715506a8eaeedd0062c04b763c1
yogesh <Yogeshgowda97@gmail.com>
2023-06-22 00:28:27 +0530

    added ses_smtp_server

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def send_email():
     if request.method == 'POST':
         # Get user input from the form
+        ses_smtp_server = request.form['ses_smtp_server']
         ses_access_key = request.form['ses_access_key']
         ses_secret_key = request.form['ses_secret_key']
         sender_email = request.form['sender_email']
@@ -16,7 +17,7 @@ def send_email():
         email_body = request.form['email_body']
 
         # Set up the SMTP connection to SES
-        smtp_server = 'email-smtp.us-east-1.amazonaws.com'  # Replace with your SES endpoint
+        smtp_server = 'ses_smtp_server'  # Replace with your SES endpoint
         smtp_port = 587  # Replace with your SMTP port
         smtp_username = ses_access_key
         smtp_password = ses_secret_key
